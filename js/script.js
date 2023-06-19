9aac38f564f8d0b7b6ab0cf61bdea2077d2c4877
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-06-19 20:57:17 +0200

    I have added CSS for mystyling and also I have made some changes to HTML, and for JS I have added function for my form validation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
+/* factory function to create players, and to get active status */
 const Player = (name, marker, isActive) => {
   const getName = () => name;
   const getMarker = () => marker;
   return { getName, getMarker, isActive };
 };
 
+/* factory gameBoard module */
 const gameBoard = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
   const cell = document.querySelectorAll(".cell");
   const getBoard = () => board;
 
+  /* function to render changes on screen for my board, and to add them to the game board variable */
   const boardRender = () => {
     cell.forEach(function (item, i) {
       item.textContent = `${board[i]}`;
@@ -16,6 +19,7 @@ const gameBoard = (() => {
     });
   };
 
+  /* function to add markers to the screen on the game board */
   const showOnBoard = () => {
     for (let i = 0; i < cell.length; i++) {
       cell[i].addEventListener("click", function () {
@@ -24,10 +28,11 @@ const gameBoard = (() => {
         boardRender();
         gameController.checkGameStatus();
       });
+      hoverBoardEffectMarker();
     }
-    boardRender();
   };
 
+  /* function to reset the game board so that players can play more than one round */
   const resetBoard = () => {
     board = ["", "", "", "", "", "", "", "", ""];
     for (let i = 0; i < cell.length; i++) {
@@ -36,6 +41,7 @@ const gameBoard = (() => {
     }
   };
 
+  /* function to update the game board, and to check if the cell is empty */
   const updateBoard = (i, value) => {
     if (board[i] !== "") {
       return false;
@@ -45,26 +51,70 @@ const gameBoard = (() => {
     }
   };
 
-  return { showOnBoard, getBoard, resetBoard };
+  /* function to add class to my .cell divs so that we can get hover effect of an active marker on the screen, when we hover over the board. */
+  const hoverBoardEffectMarker = () => {
+    for (let i = 0; i < cell.length; i++) {
+      cell[i].classList.remove("cell-o");
+      if (playerOne.isActive === true) {
+        cell[i].classList.add("cell-x");
+      } else {
+        cell[i].classList.remove("cell-x");
+        cell[i].classList.add("cell-o");
+      }
+    }
+  };
+
+  return { showOnBoard, getBoard, resetBoard, hoverBoardEffectMarker };
 })();
 
+/* displayController module */
 const displayController = (() => {
+  /* DOM */
   const gameStatusText = document.querySelector(".game-status-text");
   const boardContainer = document.querySelector(".board-container");
+  const mainSection = document.querySelector(".game-container");
   const newPlayerInputsContainer = document.querySelector(".player-container");
+  const playerScoresHeading = document.querySelector(".player-scores-heading");
   const inputPlayerOneName = document.querySelector("#first-player-name");
   const inputPlayerTwoName = document.querySelector("#second-player-name");
-  const submitPlayerButton = document.querySelector(".submit");
+  const submitPlayerButton = document.querySelector(".submit-button");
   const newRoundButton = document.querySelector(".new-round-button");
   const playAgainButton = document.querySelector(".play-again-button");
 
+  /* function to assign names to the players. */
   const createPlayer = () => {
     playerOne = Player(`${inputPlayerOneName.value}`, "X", true);
     playerTwo = Player(`${inputPlayerTwoName.value}`, "O", false);
-
-    return playerOne, playerTwo;
   };
 
+  /* function that checks if the players have entered their names in the form, and start's the game */
+  const checkFormValidation = () => {
+    if (inputPlayerOneName.value === "" || inputPlayerTwoName.value === "") {
+      alert("Please input Player One and Player two names in order to play the game.");
+      return false;
+    } else {
+      toggleGameBoard();
+      createPlayer();
+      gameController.updatePlayerScores();
+      showPlayerScoresHeading();
+      gameBoard.showOnBoard();
+      return true;
+    }
+  };
+
+  /* function that shows heading for the player scores after the submit button was clicked */
+  const showPlayerScoresHeading = () => {
+    playerScoresHeading.style.display = "block";
+  };
+
+  /* function that toggles the game board */
+  const toggleGameBoard = () => {
+    mainSection.style.display = "flex";
+    boardContainer.style.display = "grid";
+    newPlayerInputsContainer.style.display = "none";
+  };
+
+  /* function that checks if someone one the round */
   const updateScreen = () => {
     if (playerOne.isActive === true && playerTwo.isActive === false) {
       gameStatusText.textContent = `${playerTwo.getName()} has won the round!`;
@@ -77,49 +127,51 @@ const displayController = (() => {
     }
   };
 
-  const displayTextTurn = () => {
-    if (playerOne.isActive === true) {
-      gameStatusText.textContent = `It's ${playerOne.getName()} turn.`;
-    } else {
-      gameStatusText.textContent = `It's ${playerTwo.getName()} turn.`;
-    }
-  };
-
+  /* function that checks if the round is tied. */
   const updateTieGame = () => {
     showNewRoundButton();
     return (gameStatusText.textContent = "It's a Tie Game");
   };
 
+  /* function that displays which turn it is. */
+  const displayTextTurn = () => {
+    if (playerOne.isActive === true) {
+      gameStatusText.textContent = `It's ${playerOne.getName()} turn.`;
+      gameBoard.hoverBoardEffectMarker();
+    } else {
+      gameStatusText.textContent = `It's ${playerTwo.getName()} turn.`;
+      gameBoard.hoverBoardEffectMarker();
+    }
+  };
+
+  /* function that shows button for the new round, after the current round. */
   const showNewRoundButton = () => {
-    newRoundButton.style.display = "block";
+    newRoundButton.style.visibility = "visible";
   };
 
+  /* function that hides button for the new round, after the current round. */
   const hideNewRoundButton = () => {
-    newRoundButton.style.display = "none";
+    newRoundButton.style.visibility = "hidden";
   };
 
+  /* function that hides button for the new round and displays button to play the game again. */
   const showPlayAgainButton = () => {
+    hideNewRoundButton();
     displayController.playAgainButton.style.display = "block";
   };
 
-  const toggleGameBoard = () => {
-    boardContainer.style.display = "grid";
-    newPlayerInputsContainer.style.display = "none";
-  };
-
+  /* event listener for submit button */
   submitPlayerButton.addEventListener("click", () => {
-    toggleGameBoard();
-    createPlayer();
-    gameController.updatePlayerScores();
-    gameBoard.showOnBoard();
+    checkFormValidation();
   });
 
+  /* event listener for new round button */
   newRoundButton.addEventListener("click", () => {
     boardContainer.classList.remove("disabled");
     gameController.resetGame();
     hideNewRoundButton();
   });
-
+  /* event listener for button to play again the game, and also to refresh the page. */
   playAgainButton.addEventListener("click", () => {
     location.reload();
   });
@@ -127,12 +179,14 @@ const displayController = (() => {
   return { updateScreen, displayTextTurn, updateTieGame, hideNewRoundButton, showPlayAgainButton, playAgainButton };
 })();
 
+/* gameController module */
 const gameController = (() => {
   let playerOneWinScore = 0;
   let playerTwoWinScore = 0;
   let activeMarker = "";
   let moves = 0;
 
+  /* function for switching turns */
   const switchTurn = () => {
     switch (activeMarker) {
       case "":
@@ -151,11 +205,13 @@ const gameController = (() => {
     return activeMarker;
   };
 
+  /* function to toggle active Players */
   const toggleActivePlayers = () => {
     playerOne.isActive = !playerOne.isActive;
     playerTwo.isActive = !playerTwo.isActive;
   };
 
+  /* function that resets the game (active marker, active players) that set's player one as active player so that players can play new round */
   const resetGame = () => {
     playerOne.isActive = true;
     playerTwo.isActive = false;
@@ -163,6 +219,7 @@ const gameController = (() => {
     gameBoard.resetBoard();
   };
 
+  /* winning combinations for tic tac toe */
   let winningCombinations = [
     [0, 1, 2],
     [0, 4, 8],
@@ -174,6 +231,7 @@ const gameController = (() => {
     [1, 4, 7],
   ];
 
+  /* function that checks if the game board contains winning combination */
   const checkGameStatus = () => {
     moves++;
     for (let i = 0; i < winningCombinations.length; i++) {
@@ -199,14 +257,16 @@ const gameController = (() => {
     }
   };
 
+  /* function that updates player scores on the screen */
   const updatePlayerScores = () => {
     const playerOneScore = document.querySelector(".player-one-score");
     const playerTwoScore = document.querySelector(".player-two-score");
 
-    playerOneScore.textContent = `${playerOne.getName()}: ${playerOneWinScore}`;
-    playerTwoScore.textContent = `${playerTwo.getName()}: ${playerTwoWinScore}`;
+    playerOneScore.textContent = `(X) ${playerOne.getName()}: ${playerOneWinScore}`;
+    playerTwoScore.textContent = `(O) ${playerTwo.getName()}: ${playerTwoWinScore}`;
   };
 
+  /* function that checks if the player score is equal to three if it is, then it displays the winner of the game */
   const gameOver = () => {
     const gameStatusText = document.querySelector(".game-status-text");
     if (playerOneWinScore === 3) {
